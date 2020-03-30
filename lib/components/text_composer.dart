2644c36f67675cf734058e4caab0b36d79afce76
jean <jean.leal@zup.com.br>
2020-03-30 19:52:37 -0300

    enviando imagens para o firebase

--- lib/components/text_composer.dart
+++ lib/components/text_composer.dart
@@ -1,12 +1,27 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class TextComposer extends StatefulWidget {
   @override
   _TextComposerState createState() => _TextComposerState();
+
+  TextComposer(this._sendMessage);
+
+  final Function({String text, File file}) _sendMessage;
 }
 
 class _TextComposerState extends State<TextComposer> {
   bool _isComposing = false;
+  final TextEditingController _textEditingController = TextEditingController();
+
+  void reset() {
+    _textEditingController.clear();
+    setState(() {
+      _isComposing = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,10 +31,16 @@ class _TextComposerState extends State<TextComposer> {
         children: <Widget>[
           IconButton(
             icon: Icon(Icons.photo_camera),
-            onPressed: () {},
+            onPressed: () async {
+              final File file =
+                  await ImagePicker.pickImage(source: ImageSource.gallery);
+              if (file == null) return;
+              widget._sendMessage(file: file);
+            },
           ),
           Expanded(
             child: TextField(
+              controller: _textEditingController,
               decoration:
                   InputDecoration.collapsed(hintText: "Enviar uma mensagem"),
               onChanged: (text) {
@@ -27,13 +48,21 @@ class _TextComposerState extends State<TextComposer> {
                   _isComposing = text.isNotEmpty;
                 });
               },
-              onSubmitted: (text) {},
+              onSubmitted: (text) {
+                widget._sendMessage(text: text);
+                reset();
+              },
             ),
           ),
           IconButton(
             icon: Icon(Icons.send),
             color: Colors.orange,
-            onPressed: _isComposing ? () {} : null,
+            onPressed: _isComposing
+                ? () {
+                    widget._sendMessage(text: _textEditingController.text);
+                    reset();
+                  }
+                : null,
           )
         ],
       ),
--- lib/pages/chat_screen.dart
+++ lib/pages/chat_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:chat_flutter/components/text_composer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -7,6 +12,25 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  Uuid _uuid = Uuid();
+
+  void _sendMessage({String text, File file}) async {
+    Map<String, dynamic> data = {};
+    if (file != null) {
+      StorageUploadTask uploadTask =
+          FirebaseStorage.instance.ref().child(_uuid.v1()).putFile(file);
+      StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
+      String url = await taskSnapshot.ref.getDownloadURL();
+      data['imgUrl'] = url;
+    }
+
+    if (text != null) {
+      data['text'] = text;
+    }
+
+    Firestore.instance.collection('messages').add(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +38,7 @@ class _ChatScreenState extends State<ChatScreen> {
         title: Text("Olá"),
         elevation: 0,
       ),
-      body: TextComposer(),
+      body: TextComposer(_sendMessage),
     );
   }
 }
